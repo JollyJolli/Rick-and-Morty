1a4450d4919070e6c4a55a5fccfb53434ddde27c
Jolly <83844571+JollyJolli@users.noreply.github.com>
2024-07-05 02:06:58 +0000

    A lot

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     const characterContainer = document.getElementById("characterContainer");
     const searchInput = document.getElementById("searchInput");
-    const themeButton = document.getElementById("themeButton");
-    const themeStylesheet = document.getElementById("themeStylesheet");
-    const loader = document.getElementById("loader"); // Obtener el elemento del loader
+    const loader = document.getElementById("loader");
+    const favoritesButton = document.getElementById("favoritesButton");
+
+    let isShowingFavorites = false;
 
     async function getRandomCharacters(count) {
-        loader.style.display = "block"; // Mostrar el loader al iniciar la carga
+        loader.style.display = "block";
 
         let randomCharacters = [];
         const totalPages = 42;
@@ -19,12 +20,12 @@ document.addEventListener("DOMContentLoaded", function () {
             randomCharacters.push(data.results[randomCharacterIndex]);
         }
 
-        loader.style.display = "none"; // Apagar el loader cuando se han mostrado todos los personajes
+        loader.style.display = "none";
         return randomCharacters;
     }
 
     async function getAllCharacters(searchTerm = "") {
-        loader.style.display = "block"; // Mostrar el loader al iniciar la carga
+        loader.style.display = "block";
 
         let allCharacters = [];
         const totalPages = 42;
@@ -35,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
             allCharacters = allCharacters.concat(data.results);
         }
 
-        loader.style.display = "none"; // Apagar el loader cuando se han mostrado todos los personajes
+        loader.style.display = "none";
         return allCharacters;
     }
 
@@ -87,11 +88,23 @@ document.addEventListener("DOMContentLoaded", function () {
                 card.appendChild(locationInfo);
             });
 
+        const favoriteButton = document.createElement("button");
+        favoriteButton.classList.add("favorite-button");
+        favoriteButton.innerHTML = '<i class="fas fa-heart"></i>';
+        favoriteButton.addEventListener("click", function () {
+            toggleFavorite(character.id);
+            favoriteButton.classList.toggle("favorited");
+        });
+        if (isFavorite(character.id)) {
+            favoriteButton.classList.add("favorited");
+        }
+        card.appendChild(favoriteButton);
+
         return card;
     }
 
     async function loadRandomCharacters(count) {
-        loader.style.display = "block"; // Mostrar el loader al iniciar la carga
+        loader.style.display = "block";
         characterContainer.innerHTML = "";
         try {
             const randomCharacters = await getRandomCharacters(count);
@@ -102,12 +115,12 @@ document.addEventListener("DOMContentLoaded", function () {
         } catch (error) {
             console.error("Error al cargar los personajes aleatorios:", error);
         } finally {
-            loader.style.display = "none"; // Apagar el loader cuando se han mostrado todos los personajes
+            loader.style.display = "none";
         }
     }
 
     async function loadAllCharacters(searchTerm = "") {
-        loader.style.display = "block"; // Mostrar el loader al iniciar la carga
+        loader.style.display = "block";
         characterContainer.innerHTML = "";
         try {
             const allCharacters = await getAllCharacters(searchTerm);
@@ -115,22 +128,31 @@ document.addEventListener("DOMContentLoaded", function () {
                 const card = createCharacterCard(character);
                 characterContainer.appendChild(card);
             });
+
+            if (allCharacters.length === 0) {
+                showNoResultsMessage();
+            }
         } catch (error) {
             console.error("Error al cargar los personajes:", error);
         } finally {
-            loader.style.display = "none"; // Apagar el loader cuando se han mostrado todos los personajes
+            loader.style.display = "none";
         }
     }
 
     async function searchCharacters(searchTerm) {
-        loader.style.display = "none"; // Mostrar el loader al iniciar la búsqueda
-        characterContainer.innerHTML = ""; // Limpiar la lista de personajes actuales
+        loader.style.display = "none";
+        characterContainer.innerHTML = "";
 
         try {
-            const allCharacters = await getAllCharacters(); // Obtener todos los personajes
+            const allCharacters = await getAllCharacters();
             const filteredCharacters = allCharacters.filter(character =>
                 character.name.toLowerCase().includes(searchTerm.toLowerCase())
             );
+
+            if (filteredCharacters.length === 0) {
+                showNoResultsMessage();
+                return;
+            }
 
             filteredCharacters.forEach(character => {
                 const card = createCharacterCard(character);
@@ -139,28 +161,92 @@ document.addEventListener("DOMContentLoaded", function () {
         } catch (error) {
             console.error("Error al buscar los personajes:", error);
         } finally {
-            loader.style.display = "none"; // Apagar el loader cuando se han mostrado todos los personajes
+            loader.style.display = "none";
         }
     }
 
-    loadRandomCharacters(15);
+    function showNoResultsMessage() {
+        const message = document.createElement("p");
+        message.textContent = "No se encontraron resultados.";
+        characterContainer.appendChild(message);
+    }
+
+    function toggleFavorite(characterId) {
+        const favorites = getFavorites();
+        const index = favorites.indexOf(characterId);
+
+        if (index !== -1) {
+            favorites.splice(index, 1);
+        } else {
+            favorites.push(characterId);
+        }
+
+        saveFavorites(favorites);
+    }
+
+    function isFavorite(characterId) {
+        const favorites = getFavorites();
+        return favorites.includes(characterId);
+    }
+
+    function saveFavorites(favorites) {
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+    }
+
+    function getFavorites() {
+        const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+        return favorites;
+    }
+
+    async function showFavorites() {
+        isShowingFavorites = true;
+        characterContainer.innerHTML = "";
+        const favorites = getFavorites();
+
+        if (favorites.length === 0) {
+            const message = document.createElement("p");
+            message.textContent = "No tienes personajes favoritos.";
+            characterContainer.appendChild(message);
+            return;
+        }
+
+        for (let i = 0; i < favorites.length; i++) {
+            try {
+                const response = await fetch(`https://rickandmortyapi.com/api/character/${favorites[i]}`);
+                const character = await response.json();
+                const card = createCharacterCard(character);
+                characterContainer.appendChild(card);
+            } catch (error) {
+                console.error(`Error al obtener el personaje favorito ${favorites[i]}:`, error);
+            }
+        }
+    }
+
+    favoritesButton.addEventListener("click", function () {
+        if (!isShowingFavorites) {
+            showFavorites();
+            favoritesButton.textContent = "Mostrar Todos";
+        } else {
+            characterContainer.innerHTML = "";
+            loadRandomCharacters(23);
+            favoritesButton.textContent = "Mostrar Favoritos";
+        }
+        isShowingFavorites = !isShowingFavorites;
+    });
+
+    loadRandomCharacters(23);
 
     searchInput.addEventListener("input", function () {
         const searchTerm = searchInput.value.trim();
         if (searchTerm !== "") {
             searchCharacters(searchTerm);
         } else {
-            loadRandomCharacters(15);
-        }
-    });
-
-    themeButton.addEventListener("click", function () {
-        if (themeStylesheet.getAttribute("href") === "styles.css") {
-            themeStylesheet.href = "light-theme.css";
-            themeButton.innerHTML = '<i class="fas fa-moon"></i> Cambiar a Modo Oscuro';
-        } else {
-            themeStylesheet.href = "styles.css";
-            themeButton.innerHTML = '<i class="fas fa-adjust"></i> Cambiar a Modo Claro';
+            if (isShowingFavorites) {
+                characterContainer.innerHTML = "";
+                showFavorites();
+            } else {
+                loadRandomCharacters(20);
+            }
         }
     });
 });
